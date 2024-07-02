0a6312db4c15a9a5d8c275a5a8da01deb53d8ba7
Luiz Eduardo <l.eduardo8316@gmail.com>
2024-07-02 17:24:27 -0300

    feat: add config module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -2,17 +2,18 @@ import { Module } from '@nestjs/common'
 import { HttpModule } from './http/http.module'
 import { DatabaseModule } from './database/database.module'
 import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     HttpModule,
     DatabaseModule,
-    MongooseModule.forRoot(
-      'mongodb://docker:docker@localhost:27017/nest-mongo',
-      {
-        authSource: 'admin',
-      },
-    ),
+    MongooseModule.forRoot(`${process.env.DATABASE_URL}`, {
+      authSource: 'admin',
+    }),
   ],
   controllers: [],
   providers: [],
